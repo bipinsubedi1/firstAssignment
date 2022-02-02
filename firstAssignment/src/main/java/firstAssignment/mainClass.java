7b2d3bb042acc22025208d2543fcc31e7134a577
bipin <bipin@192.168.0.13>
2022-02-01 19:50:46 -0600

    My first commit.

---
+++ firstAssignment/src/main/java/firstAssignment/mainClass.java
@@ -0,0 +1,9 @@
+package firstAssignment;
+
+public class mainClass {
+public static void main (String[] args) {
+	secondClass myObj = new secondClass ();
+	myObj.setOrganization("HCL Technologies");
+	System.out.println (myObj.getOrganization());
+}
+}
---
+++ firstAssignment/src/main/java/firstAssignment/secondClass.java
@@ -0,0 +1,13 @@
+package firstAssignment;
+
+public class secondClass {
+private String organization;
+
+public String getOrganization () {
+	return organization;
+}
+
+public void setOrganization (String newOrganization){
+	this.organization = newOrganization;
+}
+}
